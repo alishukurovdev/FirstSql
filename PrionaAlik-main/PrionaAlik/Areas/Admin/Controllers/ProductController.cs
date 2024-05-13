91740c9bc7e040bf0828ef320b4c7bb74c2a159d
Zaman Safarov <zamanjs@code.edu.az>
2024-05-13 12:48:19 +0400

    jj

---
+++ PrionaAlik-main/PrionaAlik/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrionaAlik.DataAccesLayer;
+using PrionaAlik.Extensions;
+using PrionaAlik.ViewModels.Products;
+
+namespace PrionaAlik.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController(PrionaContext _context) :Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Products
+                .Select(p => new GetProductAdminVM
+                {
+                    CostPrice = p.CostPrice,
+                    Discount = p.Discount,
+                      Id = p.Id,
+                      ImageUrl = p.ImageUrl,
+                      Name = p.Name,
+                      Rating = p.Rating,
+                      SellPrice = p.SellPrice,
+                      StockCount = p.StockCount,
+
+                })
+                .ToListAsync());  
+        }
+        public async Task<IActionResult> Create()
+        {
+            return  View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductVM data)
+        {
+
+            foreach (IFormFile img in data.ImageFiles)
+            {
+                if (!img.IsValidType("image"))
+                    ModelState.AddModelError("ImageFil", "Fayil sekil formasinda olmalidi");
+            }
+           
+           foreach(IFormFile img in data.ImageFiles) {
+                if (!img.IsValidLength(200))
+                    if (!ModelState.IsValid) return View(data);
+                return View(data);
+            }
+            return View(data);
+
+        }
+
+        
+    }
+}
---
+++ PrionaAlik-main/PrionaAlik/Extensions/FileExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace PrionaAlik.Extensions
+{
+    public static class FileExtension
+    {
+
+        public static bool IsValidType(this IFormFile file, string type) => file.ContentType.Contains(type);
+
+        public static bool IsValidLength(this IFormFile file, int kb) =>
+        file.Length <= kb * 1024;
+       
+    }
+}
